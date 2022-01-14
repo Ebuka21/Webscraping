32d08175f031a8d4a476c6dbd2161114e514f717
Ebuka21 <ife_ebuka@ymail.com>
2022-01-14 23:18:27 +0100

    adjust script more

--- Final_script.py
+++ Final_script.py
@@ -7,7 +7,7 @@ import requests
 import bs4
 import lxml
 import datetime
-from datetime import datetime
+from datetime import date
 
 #base_url = "https://nigeriapropertycentre.com/for-rent/lagos/"
 
@@ -43,16 +43,22 @@ bed_number = input('how many bedrooms: ')
 
 cont = True
 
+num = 0
+
 while cont:
 
     query = f'https://nigeriapropertycentre.com/for-rent/lagos/{location.lower()}?bedrooms={bed_number}&maxprice={max_prize}&selectedLoc=1&q=for-rent+lagos+{location}+{bed_number}+bedrooms+maxprice+{max_prize}&page={page_num}'
 
+    print(query)
+    
+    requests.adapters.DEFAULT_RETRIES = 5
     search_request = requests.get(query)
     search_soup = bs4.BeautifulSoup(search_request.text, 'lxml')
 
     info = search_soup.select(".wp-block-body")
     check = len(info)
 
+    
     if check > 0:
         print('we are good to go')
         
@@ -60,31 +66,41 @@ while cont:
 
             p = ['kitchen', 'shared', 'Prepaid Meter', 'sharing', 'shared', 'parlour', 'mini flat']
             
-            search_dict['hd'+str(n)] = {
+            search_dict['hd'+str(num)] = {
                 'location':info[n].address.text.strip(' \xa0'), 
                 'price':int(info[n].select(".price")[1].text.replace(',','')), 
                 'link':'https://nigeriapropertycentre.com/'+info[n].a['href'],
-                'features':[]
+                'features':''
             }
             
             new_url = 'https://nigeriapropertycentre.com/' + info[n].a['href']
             mini_req = requests.get(new_url)
             mini_soup = bs4.BeautifulSoup(mini_req.text, 'lxml')
 
+            ft = []
             for p in p:
-                if p in mini_soup.select(".tab-body")[0].text:
-                    search_dict['hd'+str(n)]['features'].append(p)
+                if p in mini_soup.select(".tab-body")[0].text:                    
+                    ft.append(p)
+            ft_set = set(ft)
+            search_dict['hd'+str(num)]['features'] = ', '.join(ft_set)
 
+
+            num += 1
+
+        if num%10 == 0:
+            print(num)
+        
         page_num += 1
 
     else:
         print('we are not good to go')
         cont = False
 
-    period = datetime.now().ctime()
-    period = period.replace(' ', '_')
+period = str(date.today())
+#period = period.replace(' ', '_')
 
-    file_name = 'result_' + period + '.txt'
-    file = open(file_name, 'w+')
-    file.write(str(search_dict))
-    file.close()
+
+file_name = 'result_' + period + '.txt'
+file = open(file_name, 'w+')
+file.write(str(search_dict))
+file.close()
